951de5400e8abac50928f15f51825147cbecf504
Andrés Cifuentes <aecifuentesv@gmail.com>
2017-05-01 17:25:37 -0300

    Software Propotipo Funcional
    Hace tareas de login de usuario, y de ver imagenes de sitios

--- Software/app/Http/routes.js
+++ Software/app/Http/routes.js
@@ -16,5 +16,21 @@
 */
 
 const Route = use('Route')
+const User = use('App/Model/User')
 
-Route.on('/').render('welcome')
+Route.on('/').render('home')
+Route.on('/experto').render('welcome')
+.middleware('auth')
+Route.get('/test', 'ContentsController.scrape')
+.middleware('auth')
+//Route.get('/test2', 'ContentsController.renderview')
+//.middleware('auth:basic')
+Route.get('/test3', 'ContentsController.viewcontents')
+.middleware('auth')
+Route.post('/login', 'UsersController.login')
+Route.get('/logout', 'UsersController.logout')
+
+Route.get('/users', function * (request, response) {
+  const users = yield User.all() 
+  response.ok(users)
+})
